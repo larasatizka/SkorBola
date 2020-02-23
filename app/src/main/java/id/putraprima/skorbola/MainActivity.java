c2c632aafbb7280b9fbc9a538a3a9a3e0099ca3a
Putra Prima Arhandi <putraprima@gmail.com>
2020-02-23 08:06:29 +0700

    starter code

---
+++ app/src/main/java/id/putraprima/skorbola/MainActivity.java
@@ -0,0 +1,21 @@
+package id.putraprima.skorbola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO
+        //Fitur Main Activity
+        //1. Validasi Input Home Team
+        //2. Validasi Input Away Team
+        //3. Ganti Logo Home Team
+        //4. Ganti Logo Away Team
+        //5. Next Button Pindah Ke MatchActivity
+    }
+}
---
+++ app/src/main/java/id/putraprima/skorbola/MatchActivity.java
@@ -0,0 +1,18 @@
+package id.putraprima.skorbola;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match);
+        //TODO
+        //1.Menampilkan detail match sesuai data dari main activity
+        //2.Tombol add score menambahkan satu angka dari angka 0, setiap kali di tekan
+        //3.Tombol Cek Result menghitung pemenang dari kedua tim dan mengirim nama pemenang ke ResultActivity, jika seri di kirim text "Draw"
+    }
+}
